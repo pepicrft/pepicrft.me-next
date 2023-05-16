3fae1f7bfe924611fac1a9deb7ba6ff45da1ef85
Pedro Piñera <pedro@pepicrft.me>
2023-05-16 13:55:23 +0200

    Change style

--- src/pages/api/og.jsx
+++ src/pages/api/og.jsx
@@ -1,61 +1,42 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from '@vercel/og'
 
 export const config = {
   runtime: 'edge',
-};
+}
 
 export default function handler(request) {
   try {
-    const { searchParams } = new URL(request.url);
+    const { searchParams } = new URL(request.url)
 
     // ?title=<title>
-    const hasTitle = searchParams.has('title');
+    const hasTitle = searchParams.has('title')
     const title = hasTitle
       ? searchParams.get('title')?.slice(0, 100)
-      : 'My default title';
+      : 'pepicrft.me'
 
     return new ImageResponse(
       (
         <div
           style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
+            display: 'flex',
             height: '100%',
             width: '100%',
-            display: 'flex',
-            textAlign: 'center',
             alignItems: 'center',
             justifyContent: 'center',
             flexDirection: 'column',
-            flexWrap: 'nowrap',
+            backgroundImage: 'linear-gradient(to bottom, #87f4b5, #93cbf1)',
+            fontSize: 60,
+            letterSpacing: -2,
+            fontWeight: 700,
+            textAlign: 'center',
           }}
         >
           <div
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-            }}
-          >
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 120px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
+              backgroundImage: 'linear-gradient(90deg, #08203e, #524175)',
+              backgroundClip: 'text',
+              '-webkit-background-clip': 'text',
+              color: 'transparent',
             }}
           >
             {title}
@@ -65,12 +46,12 @@ export default function handler(request) {
       {
         width: 1200,
         height: 630,
-      },
-    );
+      }
+    )
   } catch (e) {
-    console.log(`${e.message}`);
+    console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
       status: 500,
-    });
+    })
   }
 }
